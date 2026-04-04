aaa7f63de73ce8e31a79e76fea4929e9c3d9cdf9
Sick <c26alexander.sick@afacademy.af.edu>
2026-04-04 11:57:03 -0600

    4/4 1156 Commit

--- instr_mem.vhdl
+++ instr_mem.vhdl
@@ -11,18 +11,58 @@ end instr_mem;
 
 -- Note: the Real RISC-V uses the ADDI for the NOP instruction, but I'm pretending 0x0000000000000000 is a NOP
 -- inserting NOPs to avoid hazards
+-- ORDER OF INSTRUCTIONS
+-- addi 1 - check
+-- la - check
+-- S
+-- S
+-- S
+-- lw
+-- LOOP: Addi
+-- S
+-- S
+-- S
+-- lw
+-- S
+-- S
+-- S
+-- add
+-- subi
+-- S
+-- S
+-- S
+-- bne
+-- S
+-- S
+-- S
+-- OUT OF LOOP: j
 architecture Behavioral of instr_mem is
     type memory_array is array (0 to 255) of STD_LOGIC_VECTOR(31 downto 0);
     signal memory : memory_array := (
         0 => x"00900293", -- addi x5, x0, 9         000000001001 00000 000 00101 0010011
-                          -- load_addr x6, array (custom instruction), where array is 0x10000000
-                          -- lw x7, 0(x6)           
-                          -- loop: addi x6, x6, 4   
-                          --       lw x10, 0(x6)    
-                          --       add x7, x10, x7 
-                          --       subi x5, x5, 1 (or   addi x5, x5, -1)  
-                          --       bne x5, x0, loop       
-        xx => x"FF9FF06F", -- done: j done            [-4; note: assumes PC is already incremented by 4]
+        1 => x"00000317", -- load_addr x6, array (custom instruction), where array is 0x10000000
+        2 => x"00000000", -- 3x Stalls
+        3 => x"00000000",
+        4 => x"00000000",
+        5 => x"00032383", -- lw x7, 0(x6)           
+        6 => x"00430313", -- loop: addi x6, x6, 4  
+        7 => x"00000000", -- 3x Stalls
+        8 => x"00000000",
+        9 => x"00000000",         
+        10 => x"00032503",--       lw x10, 0(x6) 
+        11 => x"00000000",-- 3x Stalls
+        12 => x"00000000",
+        13 => x"00000000",           
+        14 => x"007503B3",--       add x7, x10, x7 
+        15 => x"00129293",--       subi x5, x5, 1 (or   addi x5, x5, -1) 
+        16 => x"00000000",-- 3x Stalls
+        17 => x"00000000",
+        18 => x"00000000",  
+        19 => x"FA0298E3",--       bne x5, x0, loop   
+        20 => x"00000000",-- 3x Stalls
+        21 => x"00000000",
+        22 => x"00000000",     
+        23 => x"FF9FF06F",-- done: j done            [-4; note: assumes PC is already incremented by 4]
         others => (others => '0')
     );
 begin
